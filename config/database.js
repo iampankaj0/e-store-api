c8da6c8840f6b7efadefef99b89704f5a1f4f66f
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2023-01-18 16:04:57 +0530

    mongoDB connect opt added

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  mongoose.set("strictQuery", true);
+  mongoose.set("strictQuery", false);
   const { connection } = await mongoose.connect(process.env.MONGO_URI, {
     dbName: "E_Store_India",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
 
   console.log(`DataBase Connected with ${connection.host} `);
